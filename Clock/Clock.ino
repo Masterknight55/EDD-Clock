8e40663c55bd44b5bdceb1047af9fd27799beb73
Logan <lmantheyplymouth@gmail.com>
2020-01-30 10:43:56 -0800

    Added New Colors

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -1,8 +1,10 @@
 #include "FastLED.h"
  
 //Constants
+//104 LEDS
+#define NUM_LEDS2 140
 #define NUM_LEDS 144
-#define OFF_SET 35
+#define OFF_SET 40
 #define DATA_PIN 5
 
 //#define CLOCK_PIN 13
@@ -24,7 +26,7 @@ void setBlueAndTurnOff()
     }
 }
 
- void setBlue()
+ void setAllBlue()
 {
   
   for(int i=convertMinToIndex(0); convertMinToIndex(60); i++){
@@ -36,16 +38,31 @@ void setBlueAndTurnOff()
 
 void setGreenWithMin(int start, int end)
 {
+  
   for(int i=convertMinToIndex(start); i<convertMinToIndex(end); i++){
         leds[i] = CHSV(255, 255, 255);
         FastLED.show();
         //delay(50);
         }
+
+        
 }
 
-int convertMinToIndex(int min)
+void setWithMin(int start, int end, int color1, int color2, int color3)
 {
-  return ((min * (NUM_LEDS / 60)) + OFF_SET);
+  
+  for(int i=convertMinToIndex(start); i<convertMinToIndex(end); i++){
+        leds[i] = CHSV(color1, color2, color3);
+        FastLED.show();
+        //delay(50);
+        }
+
+        
+}
+
+int convertMinToIndex(double min)
+{
+  return ((min * ( (NUM_LEDS - OFF_SET) / 60.0)) + OFF_SET);
 }
 
 
@@ -53,6 +70,9 @@ int convertMinToIndex(int min)
 void loop() {
 
     
-    setBlueAndTurnOff();
+    setWithMin(0,10,255,255,255);
+    setWithMin(10,20,160,255,128);
+    setWithMin(20,30,100,100,100);
+    
     
 }
