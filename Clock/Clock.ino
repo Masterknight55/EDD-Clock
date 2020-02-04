bc9253ae86dc6c2480bdc22937a5e6e61e233261
Logan <lmantheyplymouth@gmail.com>
2020-02-04 10:14:25 -0800

    Logan Got Mad

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -10,6 +10,17 @@
 //#define CLOCK_PIN 13
  
 CRGB leds[NUM_LEDS];
+
+
+//This is a Regular Day Schedule
+int StartHour[] = {0 , 7 , 8 ,9 ,10,11,11,11,12,12,13,13};
+int StartMin[] =  {0 , 33, 26,17,8 ,0 ,25,50,15,40,4 ,54};
+
+int EndHour[] = {0 , 8, 9, 10,10,11,11,12,12,13,13};
+int EndMin[] =  {0 , 22,13, 4,56,21,46,11,36, 0,50};
+
+int CurrentMin = 15;
+int CurrentHour = 7;
  
 void setup() { 
     FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
@@ -66,13 +77,53 @@ int convertMinToIndex(double min)
 }
 
 
+int getCurrentPeroid()
+{
+
+  for(int i = 0; i < 12; i ++)
+  {
+    if (CurrentMin > StartMin[i] && CurrentMin > EndMin[i])
+    {
+      //Then it is in range of min
+      for(int k=0; k < 12; k++)
+      {
+        if (CurrentHour > StartHour[k] && CurrentHour < EndHour[k])
+        {
+          return k;
+        }
+  
+      }
+
+      }
+  }
+
+  
+}
+
+void display(int currentMin, int currentHour)
+{
+  
+//Display from current min to end of peroid
+
+if((double)EndHour[1] - (double)StartHour[1] > 1.0)
+{
+  setWithMin(currentMin,60,255,255,255);
+}
+else
+{
+  setWithMin(currentMin,EndMin[1], 255,255,255);
+}
+
+ 
+ 
+}
+
 
 void loop() {
 
+    //setWithMin(CurrentHour,CurrentHour+1, 160,255,128);
+    //setWithMin(getCurrentPeroid(),getCurrentPeroid()+1, 160,255,128);
     
-    setWithMin(0,10,255,255,255);
-    setWithMin(10,20,160,255,128);
-    setWithMin(20,30,100,100,100);
-    
+    display(CurrentMin, CurrentHour);
     
 }
